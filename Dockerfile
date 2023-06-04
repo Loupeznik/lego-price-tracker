f3300d0ba49ab9de7e46d09f44bded3a805bcca8
Dominik Zarsky <dominik.zarsky@gmail.com>
2023-06-04 16:17:32 +0200

    add docker support

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.11-alpine
+
+WORKDIR /app
+
+RUN apk add curl unzip chromium-chromedriver chromium supervisor
+
+COPY . .
+COPY infra/supervisord.conf /etc/supervisord.conf
+
+RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
+
+RUN mkdir -p /etc/cron.d/ && echo "1 6,18 * * * python /app/scheduler.py" > /etc/cron.d/scraper
+
+RUN crontab /etc/cron.d/scraper
+CMD ["/usr/bin/supervisord", "-c", "/etc/supervisord.conf"]
--- scraper.py
+++ scraper.py
@@ -17,6 +17,10 @@ from db.record import Record
 def get_driver() -> webdriver.Chrome:
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--window-size=1420,1080")
+    options.add_argument("--disable-dev-shm-usage")
     service = Service(os.environ["CHROMEDRIVER_PATH"])
     return webdriver.Chrome(options=options, service=service)
 
